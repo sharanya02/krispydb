4889f6df5a10e94f33c044ab97ddbfe606233226
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2021-01-24 12:38:03 +0530

    Delete commands directory

--- commands/assign.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'assign',
-	description: 'Assigns task to role/user',
-	execute(message, args) {
-        // message.channel.send(args);
-		message.channel.send('Assigned task.');
-	},
-};
