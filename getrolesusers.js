050ed1bcfc0bae44d34c2d077df246f19e816b26
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2021-01-24 12:43:17 +0530

    pls werk

---
+++ getrolesusers.js
@@ -0,0 +1,135 @@
+// module.exports = {
+//     hello: function() {
+//        console.log("Hello");
+//        return;
+//     },
+//     getRolesUsers: (guild) =>  {
+//         let rolemap = guild.roles.cache
+//         .sort((a, b) => b.position - a.position)
+//         .map(r => `${r.id},${r.name}`);
+//         if (rolemap.length > 1024) rolemap = "To many roles to display";
+//         if (!rolemap) rolemap = "No roles";
+        
+//         let roles = new Array();
+//         for (const role of rolemap) {
+//             let roleData = role.split(',');
+//             roles.push([roleData[0], roleData[1]]);
+//         }
+//         // roles -> a 2d array with role names and corresponding ids
+//         // database WORK, update existing users or create new
+        
+//         let users = new Array();
+//         guild.members.fetch().then(members => {
+//             members.map(member => {
+//                 users.push([member.user.id, member.user.username]);
+//             });
+//             let role_user = new Array();
+//             for (const role of roles) {
+//                 role_id = role[0];
+//                 role_user.push([role_id, new Array()]);
+//                 for (const user of users) {
+//                     user_id = user[0];
+//                     user_obj = guild.members.cache.get(user_id)
+//                     if (user_obj.roles.cache.some((role) => role.id == role_id)) {
+//                         idx = role_user.length - 1;
+//                         role_user[idx][1].push(user_id);
+//                     }
+//                 }
+//             }
+//             console.log(role_user)
+//             // role_user -> 2d array with role id and list of names
+//             // database WORK, update existing users or create new
+//         });
+//     },
+//     getRoles : (guild) => {
+//         let roles = new Array();
+//         for (const role of rolemap) {
+//             let roleData = role.split(',');
+//             roles.push([roleData[0], roleData[1]]);
+//         }
+//         return roles;
+//     },
+//     getRoleMembers : (guild, roleID) => {
+//         // let rolemap = guild.roles.cache
+//         // .sort((a, b) => b.position - a.position)
+//         // .map(r => `${r.id},${r.name}`);
+//         // if (rolemap.length > 1024) rolemap = "To many roles to display";
+//         // if (!rolemap) rolemap = "No roles";
+        
+//         let roles = getRoles(guild);
+//         // roles -> a 2d array with role names and corresponding ids
+        
+//         let users = new Array();
+//         guild.members.fetch().then(members => {
+//             members.map(member => {
+//                 users.push([member.user.id, member.user.username]);
+//             });
+//             let role_user = new Array();
+//             for (const role of roles) {
+//                 role_id = role[0];
+//                 role_user.push([role_id, new Array()]);
+//                 for (const user of users) {
+//                     user_id = user[0];
+//                     user_obj = guild.members.cache.get(user_id)
+//                     if (user_obj.roles.cache.some((role) => role.id == role_id)) {
+//                         idx = role_user.length - 1;
+//                         role_user[idx][1].push(user_id);
+//                     }
+//                 }
+//             }
+    
+//             // role_user -> 2d array with role id and list of names
+//             // database WORK, update esxisting users or create new
+    
+//             // --------- NEW STUFF
+//         });
+//     },
+//     getUsers : (guild, role) => {
+//         //if role or user no mentioned in message, role = @everyone
+//         // all 
+
+//         let users = new Array();
+//         guild.members.fetch().then(members => {
+//             members.map(member => {
+//                 users.push([member.user.id, member.user.username]);
+//             });
+//             let usersReqd = new Array();
+//             for (const user of users) {
+//                 user_id = user[0];
+//                 user_obj = guild.members.cache.get(user_id);
+//                 if (user_obj.roles.cache.some((role) => role == role)) {
+//                     usersReqd.push(user_id);
+//                 }
+//             }
+//             console.log(usersReqd);
+//             // role_user -> 2d array with role id and list of names
+//             // database WORK, update existing users or create new
+//         });
+//     }
+//  }
+
+async function getUsers (guild, role) {
+    //if role or user no mentioned in message, role = @everyone
+    // all 
+    // return await pranjal(guild, role);
+    let usersReqd = new Array();
+    let users = new Array();
+    role_id = role.id;
+    let members = await guild.members.fetch();
+    members.map(member => {
+            users.push([member.user.id, member.user.username]);
+        });
+        for (const user of users) {
+            user_id = user[0];
+            user_obj = guild.members.cache.get(user_id);
+            if (user_obj.roles.cache.some((role) => role_id == role.id)) {
+                usersReqd.push(user_id);
+            }
+        }
+        return usersReqd;
+        // role_user -> 2d array with role id and list of names
+        // database WORK, update existing users or create new
+    };
+
+
+module.exports = getUsers;
---
+++ index.js
@@ -0,0 +1,133 @@
+const db = require('./database/connection')
+require('dotenv').config();
+require('./models/relations')
+
+db.authenticate()
+  .then(() => {
+    console.log('Connected To Database')
+  })
+  .catch(err => {
+    console.log('An error occurred' + err)
+    process.exit(2)
+  })
+
+const getInfo = require('./getrolesusers');
+
+const fs = require('fs');
+const Discord = require('discord.js');
+
+const { prefix } = require('./config.json');
+const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
+const { Console } = require('console');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+// On adding to a new server
+client.on('guildCreate', guild => {
+	
+	console.log(guild.id);
+	console.log(guild.name);
+	updateRolesUsers(guild);
+
+});
+
+// On adding new member
+client.on("guildMemberAdd" ,(message, member) => {
+    message.channel.send("Welcome")
+});
+
+// On message
+client.on('message', async message => {
+	
+	if (!message.content.startsWith(prefix) || message.content.startsWith(`${prefix}${prefix}`) || message.author.bot) 
+	return;
+	
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+	
+	switch(command){
+
+		case 'ping':
+		case 'check':
+		case 'online':
+		case 'checkonline':
+			client.commands.get('ping').execute(message, args);
+			break;
+
+		case 'help':
+		case 'commands':
+			client.commands.get('help').execute(message, args);
+			break;
+
+		case 'assign':
+		case 'give':
+		case 'givetask':
+		case 'assigntask':
+			client.commands.get('assign').execute(message, args);
+			break;
+
+		case 'show':
+		case 'showtask':
+		case 'display':
+		case 'displaytask':
+			client.commands.get('show').execute(message, args);
+			break;
+
+		case 'done':
+		case 'donetask':
+		case 'finished':
+		case 'finishedtask':
+			client.commands.get('done').execute(message, args);
+			break;
+
+		case 'progress':
+		case 'taskprogress':
+			client.commands.get('progress').execute(message, args);
+			break;
+
+		case 'update':
+		case 'refresh':
+			// getInfo.getUsers(message.guild, message.mentions.roles.first());
+			let x = await getInfo.getUsers(message.guild, message.mentions.roles.first());
+			console.log(x);
+			break;
+
+		case 'remove':
+		case 'delete':
+			client.commands.get('remove').execute(message, args);
+			break;
+
+
+		case 'server':
+			message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+			break;
+
+		case 'user-info':
+			message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+			break;
+		
+		default:
+			message.channel.send(`What are you sayin' bro?? That's not a command!`);
+			message.channel.send(`Use \`~help\` to know the commands.`);
+			break;
+	}
+      
+});
+
+client.login(process.env.TOKEN);
+
+client.once('ready', () => {
+	console.log('Logged in');	
+});
+client.on('shardError', error => {
+	console.error('A websocket connection encountered an error:', error);
+});
